7ac82542f4f255478bf539608d6fa95e6a5a5181
Sky Xu <skyrocks88xmz@gmail.com>
2017-10-29 22:19:32 -0700

    finish unique count

--- Source/histogram.py
+++ Source/histogram.py
@@ -8,7 +8,7 @@ with open('/usr/share/dict/words', 'r') as f:
 raw_sentence = "one fish two fish red fish blue fish"
 sentence = raw_sentence.split()
 list_result = []
-
+group_count = []
 def histogram_list(sentence):
 # stores each unique word and number of times the word appears
 #1 as a list of list
@@ -48,7 +48,7 @@ histogram_dict(sentence)
 # group_list = [(1, ['one', 'two', 'red', 'blue']), (4, ['fish'])]
 def group_histogram(sentence):
     flattened_list = []
-    group_count = []
+    # group_count = []
     for item in sentence:
         occurence = sentence.count(item)
         if item not in flattened_list:
@@ -61,25 +61,35 @@ def group_histogram(sentence):
     for current_index in range(1, len(flattened_list), 2):
         # if occurence already exist in group_count, get the words that has the same occurence
         if flattened_list[current_index] in group_count:
+            # get the duplicated frequency of words
             temp_count = flattened_list[current_index]
+            # get the position of the frequency's matching dict words
             append_word_index = group_count.index(temp_count) + 1
+            # get the word in flattened list with the same frequency
             append_word = flattened_list[current_index-1]
+            # append the word to dict in the result list
             group_count[append_word_index].append(append_word)
-            # print(group_count)
         else:
             # append the tuple with the unique occurence as first element, dict of words as second
             # group_count.append((flattened_list[current_index], flattened_list[current_index-1]))
             group_count.append(flattened_list[current_index])
             group_count.append([flattened_list[current_index-1]])
-            # print('OOOOOOOOO', group_count)
-    print(group_count)
+    return(group_count)
 
 group_histogram(sentence)
 
+print("YOOOOOO", group_count)
+def unique_words(group_count):
+    print("HII", len(group_count))
+    for i in range(0,len(group_count)):
+        print(group_count[i])
+        if group_count[i] == 1:
+            result_word = group_count[i+1]
+        unique_words_count = len(result_word)
+    print(unique_words_count)
+    return unique_words_count
 
-
-
-
+unique_words(group_count)
 
 
 
