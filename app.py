511098469d18ad6be2c74a1d7fe0428becac73d0
Sky Xu <skyrocks88xmz@gmail.com>
2017-12-25 16:07:59 -0500

    push to heroku

--- app.py
+++ app.py
@@ -1,11 +1,15 @@
 from flask import Flask
 from sample import *
+from markov import *
+from grab_clean_file import *
 
 app = Flask(__name__)
+cleaned_file = read_file()
 
 @app.route("/<int:population>")
-def create(population=1):
-    return create_random_sentence(population)
+def create(population=15):
+    sentence = Markov(cleaned_file).generate_sentence(population)
+    return sentence
 
 if __name__ == '__main__':
     app.run()
