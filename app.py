9909da1d92fc7bf8a2c033dafbeda1e011c82f77
Sky Xu <skyrocks88xmz@gmail.com>
2017-12-25 16:10:40 -0500

    fix

--- app.py
+++ app.py
@@ -4,10 +4,10 @@ from markov import *
 from grab_clean_file import *
 
 app = Flask(__name__)
-cleaned_file = read_file()
 
 @app.route("/<int:population>")
-def create(population=15):
+def create(population=10):
+    cleaned_file = read_file()
     sentence = Markov(cleaned_file).generate_sentence(population)
     return sentence
 
