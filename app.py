09524bd5cace92574356bc582032b93c275bca3e
Sky Xu <skyrocks88xmz@gmail.com>
2018-01-04 16:35:21 -0800

    finish

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from sample import *
 from markov import *
 from grab_clean_file import *
 from flask import render_template
+import twitter
 
 app = Flask(__name__)
 
@@ -12,11 +13,17 @@ def home():
     random_sentence = Markov(cleaned_file).generate_sentence()
     return render_template('index.html', sentence=random_sentence)
 
-@app.route("/<int:population>")
-def create(population=10):
-    cleaned_file = read_file()
-    sentence = Markov(cleaned_file).generate_sentence(population)
-    return sentence
+@app.route('/tweet', methods=['POST'])
+def tweet():
+    status = request.form['sentence']
+    twitter.tweet(status)
+    return redirect('/')
+
+# @app.route("/<int:population>")
+# def create(population=10):
+#     cleaned_file = read_file()
+#     sentence = Markov(cleaned_file).generate_sentence(population)
+#     return sentence
 
 if __name__ == '__main__':
     app.run()
---
+++ twitter.py
@@ -0,0 +1,18 @@
+import dotenv
+from requests_oauthlib import OAuth1Session
+
+consumer_key = "QSvRQvhgc2G8S6pOGnA2aUKk1"
+consumer_secret = "zKBtynk4EbsYmM7skdNBiQHHh4es2VwyztdFnyARVLJyDpIaxO"
+access_token = "949074237968465920-Om8Fx5I6HFBazyD1is0fhlSLEt0xW7S"
+access_token_secret = "VebiPHFilNvwPzWOiEfsDO75wFLwpcdKqzWo1IJq95zfL"
+
+session = OAuth1Session(consumer_key,
+                        client_secret=consumer_secret,
+                        resource_owner_key=access_token,
+                        resource_owner_secret=access_token_secret)
+url = 'https://api.twitter.com/1.1/statuses/update.json'
+
+
+def tweet(status):
+    resp = session.post(url, { 'status': status })
+    return resp.text
